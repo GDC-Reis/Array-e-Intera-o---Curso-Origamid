1ab8cfa0aa48767062c79a49a4d179e17f0b14dd
Gustavo Backup <gustavobackupcode@gmail.com>
2024-07-27 23:35:06 -0300

    Commit realizado com a continuação da aula (Aula 2)

--- array.js
+++ array.js
@@ -217,8 +217,125 @@ total2; // 165
 
 const numeros2 = [10, 25, 60, 5, 35, 10];
 
-const maiorValor = numeros.reduce((anterior, atual) => {
+const maiorValor = numeros2.reduce((anterior, atual) => {
     return anterior < atual ? atual : anterior;
 });
 
 maiorValor; // 60
+
+
+
+// Podemos retornar outros valores
+
+const aulas3 = [
+    {
+        nome: 'HTML 1',
+        min: 15,
+    },
+    {
+        nome: 'HTML 2',
+        min: 10,
+    },
+    {
+        nome: 'CSS 1',
+        min: 20,
+    },
+    {
+        nome: 'JS 1',
+        min: 25,
+    },
+]
+
+const listaAulas = aulas3.reduce((acumulador, aula, index) => {
+    acumulador[index] = aulas3.nome;
+    return acumulador;
+}, {}); //{} -> Objeto Vazio 
+
+
+/* 
+
+    Passo a passo Reduce
+    Passo a passo do método reduce criando um OBJETO
+
+    // 1
+    {} -> Acumulador, {nome: 'HTML 1', min: 15} -> Array percorrido, 0 -> Index
+
+    aulas.reduce(({}, {nome: 'HTML 1', min: 15}, 0) => {
+        {}[0] = 'HTML 1';
+        return {0: 'HTML1'};
+    }, {});
+
+    
+    // 2
+    aulas.reduce(({0: 'HTML 1'}, {nome: 'HTML 2', min : 10}, 1) => {
+        {0: 'HTML 1'}[1] = 'HTML 2';
+        return {0: 'HTML 1', 1: 'HTML 2'};
+    }, {});
+
+
+    // 3
+    aulas.reduce(({0: 'HTML 1', 1: 'HTML 2'}, {nome: 'CSS 1', min : 20}, 2) => {
+        {0: 'HTML 1', 1: 'HTML 2'}[2] = 'CSS 1';
+        return {0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1'};
+    }, {});
+
+
+    // 4
+    aulas.reduce(({0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1'}, {nome: 'JS 1', min : 25}, 3) => {
+        {0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1'}[3] = 'JS 1';
+        return {0: 'HTML 1', 1: 'HTML 2', 2: 'CSS 1', 3: 'JS 1'};
+    }, {});
+
+*/
+
+
+// [].reduceRight()
+// Existe também o método '[].reduceRigth()', a diferença é que este começa a iterar da direita para a esquerda,
+// enquanto o reduce itera da esquerda para a direita.
+
+const frutas = ['Banana', 'Pêra', 'Uva'];
+
+const frutasRigth = frutas.reduceRight((acc, fruta) => {
+    acc + ' ' + fruta;
+})
+
+const frutasLeft = frutas.reduce((acc, fruta) => {
+    acc + ' ' + fruta;
+})
+
+frutasRigth; // Uva Pêra Banana
+frutasLeft; // Banana Pêra Uva
+
+
+
+// [].some()
+// '[].some()', se pelo menos um return da iteração for truthy (caso seja algum valor que consiga ser retornado em verdadeiro), ele retorna true.
+
+const frutas2 = ['Banana', 'Pêra', 'Uva'];
+const temUva = frutas2.some((fruta) => {
+    return fruta === 'Uva';
+}); // true
+
+
+function maiorQue100(numero) {
+    return numero > 100;
+}
+
+const numeros3 = [0, 43, 22, 88, 101, 2];
+const temMaior = numeros3.some(maiorQue100); // true
+
+
+
+// [].every()
+// '[].every()', se todos os returns das iterações forem truthy, o método irá retornar true.
+// Se pelo menos um for falsy, ele irá retornar false.
+
+const frutas3 = ['Banana', 'Pêra', 'Uva', ''];
+// False pois pelo menos uma fruta está vazia '', o que é um valor falsy
+
+const arrayCheias = frutas3.every((fruta) => {
+    return fruta; // false
+});
+
+const numeros4 = [6, 43, 22, 88, 101, 29];
+const maiorQue3 = numeros4.every( x => x > 3); // true
